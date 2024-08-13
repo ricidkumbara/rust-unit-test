d40d33a4886f5fdd9133e24f7cb91391e3c50131
ricidkumbara <ricidkumbara@gmail.com>
2024-08-14 06:40:52 +0700

    init

---
+++ src/main.rs
@@ -0,0 +1,88 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn start_application(host: &str, port: u16) {
+    if host == "localhost" {
+        panic!("You cant use localhost as host");
+    } else {
+        println!("Starting application on {} {}", host, port);
+    }
+}
+
+/* #[test]
+fn test_simple() {
+    println!("Hello Test");
+}
+
+#[test]
+fn test_add() {
+    let result = add(1, 2);
+    assert_eq!(result, 3, "Should result 3");   
+}
+
+#[test]
+#[should_panic]
+fn test_start_application() {
+    start_application("localhost", 80);
+}
+
+#[test]
+#[ignore]
+fn test_ignore() {
+    println!("This test is ignored");
+}
+
+#[test]
+fn test_add_again() -> Result<(), String> {
+    let result = add(1, 2);
+
+    if result == 3 {
+        Ok(())
+    } else {
+        Err("1 + 2 should be 3".to_string())
+    }
+} */
+
+#[cfg(test)]
+mod tests {
+    use crate::{add, start_application};
+    
+    #[test]
+    fn test_simple() {
+        println!("Hello Test");
+    }
+
+    #[test]
+    fn test_add() {
+        let result = add(1, 2);
+        assert_eq!(result, 3, "Should result 3");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_start_application() {
+        start_application("localhost", 80);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_ignore() {
+        println!("This test is ignored");
+    }
+
+    #[test]
+    fn test_add_again() -> Result<(), String> {
+        let result = add(1, 2);
+
+        if result == 3 {
+            Ok(())
+        } else {
+            Err("1 + 2 should be 3".to_string())
+        }
+    }
+}
